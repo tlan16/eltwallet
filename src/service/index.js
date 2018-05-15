e0c5e560133a68e9a6ac6f74c5e1d87e0863b364
kathy y <meimeiy1121@gmail.com>
2018-05-15 13:25:59 +1000

    clean up service

--- src/service/index.js
+++ src/service/index.js
@@ -1,38 +1,45 @@
 import request from 'superagent';
 import { fetchMessages, receiveMessages, failToReceive } from '../actions';
-const base_url = 'http://localhost:3000';
+const base_url = `http://${window.location.hostname}:3000`;
 
 export const messageService = store => next => action => {
   next(action);
   switch (action.type) {
     case 'FETCH_MESSAGES_START':
-      const fetch_messages_url = `${base_url}/api/message/recipient/${
-        action.address
-      }`;
-      request.get(fetch_messages_url).end((err, res) => {
-        if (err) {
-          return next(failToReceive(err));
-        }
-        const messages = JSON.parse(res.text);
-        next(receiveMessages(messages));
-      });
+      fetchMessagesStart(action.address, receiveMessages, next);
       break;
     case 'SELECT_MESSAGE':
-      const message = { ...action.selectedMessage, read_at: new Date() };
-      const message_uuid = action.selectedMessage.uuid;
-      const mark_message_as_read_url = `${base_url}/api/message/mark-read/${message_uuid}`;
-      console.log(mark_message_as_read_url);
-      request
-        .put(mark_message_as_read_url)
-        .set('Content-Type', 'json')
-        .end((err, res) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log(res);
-        });
+      markMessageAsRead(action.selectedMessage);
       break;
+    case 'SEND_MESSAGE':
+
     default:
       break;
   }
+};
+
+const fetchMessagesStart = (address, receiveMessages, next) => {
+  const fetch_messages_url = `${base_url}/api/messages/recipient/${address}`;
+  request.get(fetch_messages_url).end((err, res) => {
+    if (err) {
+      return next(failToReceive(err));
+    }
+    const messages = JSON.parse(res.text);
+    next(receiveMessages(messages));
+  });
+};
+
+const markMessageAsRead = selectedMessage => {
+  const message = { ...selectedMessage, read_at: new Date() };
+  const message_uuid = selectedMessage.uuid;
+  const mark_message_as_read_url = `${base_url}/api/message/mark-read/${message_uuid}`;
+  request
+    .put(mark_message_as_read_url)
+    .set('Content-Type', 'json')
+    .end((err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
 };
